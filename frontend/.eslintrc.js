8124eb97da80dda6b5538b038105845686b38a7c
makedonskyy <yaroslavamakedonskaya@yandex.ru>
2023-08-03 16:00:25 +0300

    fix style

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true
-    },
-    extends: ['@nuxtjs/eslint-config-typescript'],
-    plugins: [],
-    rules: {
-        'vue/no-multiple-template-root': 0,
-        '@typescript-eslint/no-var-requires': 0
-    }
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['@nuxtjs/eslint-config-typescript'],
+  plugins: [],
+  rules: {
+    'vue/no-multiple-template-root': 0,
+    '@typescript-eslint/no-var-requires': 0
+  }
 }
